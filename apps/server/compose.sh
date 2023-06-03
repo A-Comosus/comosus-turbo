2928172499c5175356b52bd5be90cdcc016e5be6
norriswu0 <norris.wu.au@outlook.com>
2023-06-03 13:50:44 +1000

    add docker compose initialization script

--- apps/server/compose.sh
+++ apps/server/compose.sh
@@ -1,8 +1,31 @@
-docker compose up -d
+INIT=false
+
+while getopts ":ih" option; do
+  case $option in
+      i) # display Help
+        INIT=true
+        ;;
+      h)
+        echo "There's no help, you are on your own!"
+        exit
+        ;;
+      *)
+        echo "There's no help, you are on your own!"
+        exit
+        ;;
+  esac
+done
+
+compose() {
+  docker compose up -d
+}
 
 initReplica() {
   docker compose exec -it cockroach-0 ./cockroach init --insecure
 }
 
-## Intentionally ignore error here because not sure how to handle it yet. :shrug:
-initReplica || true
+compose
+
+if [ "$INIT" == true ]; then
+  initReplica
+fi
